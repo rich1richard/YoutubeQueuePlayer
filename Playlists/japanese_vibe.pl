b1b56bd21e012875eefb9a56234de21a9deeb075
rich1richard <gamedevrichard@gmail.com>
2025-11-16 22:19:40 +0100

    Initial Commit

---
+++ Playlists/japanese_vibe.pl
@@ -0,0 +1,102 @@
+unravel - tk
+red out - kenshi yonezu
+Creepy Nuts  - ばかまじめ
+美波 - Prologue
+FUNKIST - Ft.
+BUMP OF CHICKEN - Hello,world!
+Survive Said The Prophet - Paradox
+LiSA  - 再会 (produced by Ayase)
+MY FIRST STORY - REVIVER
+sakanaction - アイデンティティ
+FUNKIST - Snow Fairy
+FLOW - Sign
+大原ゆい子 - 旅人の唄
+Hitorie - 3分29秒
+YOSHIKI  - Red Swan
+QUEEN BEE - 01
+BUMP OF CHICKEN - 新世界
+SiM - The Rumbling (TV Size)
+EGOIST - 英雄　運命の詩
+Linked Horizon - Shokei to Shikabane no Michi - TV Size
+Kenshi Yonezu  - JANE DOE
+MAN WITH A MISSION - My Hero
+AiNA THE END - Red:birthmark
+majiko - Kokoronashi
+ꉈꀧ꒒꒒ꁄꍈꍈꀧ꒦ꉈ ꉣꅔꎡꅔꁕꁄ  - W●RK - King Gnu Dome Tour THE GREATEST UNKNOWN at TOKYO DOME
+Cö shu Nie - 絶体絶命
+TK from Ling tosite sigure - 蝶の飛ぶ水槽 (sainou mix)
+PornoGraffitti - サウダージ
+NICO Touches the Walls - ホログラム
+sakanaction - Kaiju
+Gen Hoshino - Comedy
+TOMONARI SORA - 宵祭り - Yoimatsuri
+Jin Hashimoto - STAND PROUD
+Omoinotake - EVERBLUE
+Ayase - 幽霊東京
+natori - メーベル
+ryo (supercell)  - takt
+ano - ちゅ、多様性。
+amazarashi - 境界線
+Lilas - スパークル
+Kiro Akiyama - アイデンティティ
+MADKID - FAITH
+TK from Ling tosite sigure - haze
+TOMONARI SORA - 睨めっ娘
+MYTH & ROID - TIT FOR TAT
+Da-iCE - I wonder
+Fear, and Loathing in Las Vegas - Just Awake
+本間芽衣子（CV.茅野愛衣）  - secret base ～君がくれたもの～ (10 years after Ver.)
+Kenshi Yonezu - 愛麗絲（正式表記はAliceの中国語簡体字表記となります。）
+TK from Ling tosite sigure - Fantastic Magic
+3年E組うた担(渚&茅野&業&磯貝&前原) - バイバイ YESTERDAY
+ROOKiEZ is PUNK'D - IN MY WORLD
+tuki. - 純恋愛のインゴット - Pure Love Ingot
+Creepy Nuts - パッと咲いて散って灰に
+krage - request
+Kenshi Yonezu - 打上花火
+Kanaria - BRAIN
+TK from Ling tosite sigure - unravel - From THE FIRST TAKE
+tuki. - ひゅるりらぱっぱ - HYURURIRAPAPPA
+King Gnu - どろん
+ASIAN KUNG-FU GENERATION - リライト
+MY FIRST STORY  - 1,000,000 TIMES feat. chelly (EGOIST)
+Rokudenashi - イオ
+YOASOBI - 怪物
+須田景凪 - veil
+King Gnu - BOY
+LiSA  - ReawakeR (feat. Felix of Stray Kids)
+Kenshi Yonezu - 感電
+WEDNESDAY CAMPANELLA - Summer Time Ghost
+OFFICIAL HIGE DANDISM - Cry Baby
+cinema staff - Great Escape - Alternate Ver.
+Creepy Nuts - オトノケ - Otonoke
+Hitsujibungaku - Burning
+DISH// - 猫
+ReoNa - ないない
+sakanaction - ネイティブダンサー
+桜島麻衣(CV:瀬戸麻沙美) - 不可思議のカルテ 桜島麻衣 Ver.
+GALNERYUS - HUNTING FOR YOUR DREAM! - TV Size
+Yoko Takahashi - 残酷な天使のテーゼ (Directors Edit Version)
+Reol - 第六感
+Ayase - 夜撫でるメノウ
+Cö shu Nie - give it back
+OFFICIAL HIGE DANDISM - I Love...
+Amamiya Sora - Skyreach
+AiNA THE END - 渇望
+ヨルシカ - 晴る
+Sheena Ringo - 白日のもと
+AKASAKI - ルーツ
+back number - 幕が上がる
+ZUTOMAYO - Nouriueno Cracker
+Kenshi Yonezu - MAD HEAD LOVE
+imase - ユートピア
+ZUTOMAYO - TAIDADA
+Fujii Kaze - きらり
+Hikaru Utada  - 二時間だけのバカンス
+Fujii Kaze - damn
+back number - ハッピーエンド
+ヨルシカ - 火星人
+back number - 水平線
+Fujii Kaze - Prema
+ヨルシカ - 修羅
+AKASAKI - 全身前礼
---
+++ Playlists/modern_prog_metal.pl
@@ -0,0 +1,380 @@
+
+Periphery - Periphery - Wax Wings (Official Audio)
+Periphery - Periphery - It's Only Smiles (Audio)
+SUMERIAN - PERIPHERY - Scarlet (Official Music Video)
+Dead by April Official - Dead by April | Self Deception — Feeding Demons
+Self Deception - Self Deception - Smoke You Out (OFFICIAL LYRIC VIDEO)
+Better Noise Music - NOTHING MORE - IF IT DOESN'T HURT (Official Music Video)
+Architects - Architects - "Curse"
+DAMN NATION - DAMN NATION - 'Apocalypse' (Official Music Video)
+coldrain - BREATHE
+Uuden Musiikin Kilpailu - Blind Channel - Dark Side (Lyric Video) // UMK21
+Better Noise Music - Hyro The Hero - FU2 ft. AJ Channer of Fire From The Gods (Official Video)
+3 Pill Morning - 3 Pill Morning - All Before (OFFICIAL MUSIC & LYRIC VIDEO)
+SBG Records - Rain City Drive - "Witch Hunt"
+SharpTone Records - Of Mice & Men - Obsolete (Official Music Video)
+Dennis Kurc - Dennis Kurc - Be Adrift
+Nik Nocturnal - Topic - Architect
+Arising Empire - ANNISOKAY - Ultraviolet (OFFICIAL VISUALIZER)
+FEVER 333 - FEVER 333 - $wing [Official Video]
+Linkin Park - Forgotten - Linkin Park (Hybrid Theory)
+limpbizkit - Limp Bizkit - Rollin' (Air Raid Vehicle)
+Attack Attack! - Attack Attack! - Fade With Me (Official Video)
+WAKE UP HATE - WAKE UP HATE - I Just Don't Love You Anymore
+WAKE UP HATE - Wake Up Hate - SINK OR SWIM (Official Visualizer)
+Revivalrecs - Eyes Set To Kill - Face The Rain [feat. Howard Jones] (Official Music Video)
+I Built The Sky - Metal Zone 🤘
+Neural DSP - I Built The Sky - Rage Against Gojira - Archetype: Gojira
+I Built The Sky - I Built The Sky - Up Into The Ether (watch in 4k)
+jamies elsewhere - Jamie's Elsewhere "Capital Vices" ft. Phil Druyor & Nick Sampson
+Story Of The Year - War
+Avenged Sevenfold - Avenged Sevenfold - Almost Easy [Official Music Video]
+riserecords - Memphis May Fire - Better Things
+otherwisetube - OTHERWISE - "Bad Trip" (Defy)
+Long Branch Records - Siamese - On Fire (Official Music Video)
+The Effect - The Effect - Unwanted (Official Music Video)
+Sakoya - Sakoya - Wandering (Official Music Video)
+Lauren Babic - SKYLIMIT – "A Place You'll Never Find" (Official Music Video)
+Dead Eyes - Dead Eyes - Avalanche (Official Single Stream)
+Mr. Big - Mr. Big - Shine (MV)
+dream drop - DREAM DROP - DIRT (OFFICIAL VIDEO)
+Hollywood Undead - Hollywood Undead - Evil (Official Video)
+RainCityDrive - Rain City Drive - "Dying For" (Visualizer)
+Arising Empire - FOR THE FALLEN DREAMS - No Heaven (OFFICIAL VIDEO)
+Self Deception - Self Deception - The Scandinavian Dream (OFFICIAL LYRIC VIDEO)
+SharpTone Records - Caskets - The Only Ones (Official Music Video)
+Saint Asonia - Saint Asonia - Devastate
+The Word Alive - The Word Alive - Nocturnal Future [Official Music Video]
+Living Colour - Living Colour - Cult Of Personality (Official Video)
+Out Of Line Music - NOVELISTS - Turn It Up (Official Music Video)
+Out Of Line Music - NOVELISTS - Terrorist (Official Music Video)
+Out Of Line Music - NOVELISTS - The Answer (Official Visualizer)
+Out Of Line Music - NOVELISTS - Heretic (feat. LANDMVRKS) (Official Music Video)
+Out Of Line Music - NOVELISTS - Smoke Signals (Official Music Video)
+ENMY - ENMY - Fake (Official Music Video)
+Tremonti - Tremonti - If Not For You (Official Music Video)
+Mark Morton - Mark Morton - Sworn Apart ft. Jacoby Shaddix
+Better Noise Music - HELLYEAH - Oh My God (Official Music Video)
+SolenceOfficial - Solence - Heaven [OFFICIAL VIDEO]
+Five Finger Death Punch - Five Finger Death Punch - Leave It All Behind (Official Audio)
+Egypt Central - Egypt Central - Raise the Gates (Official Audio)
+Asking Alexandria - Asking Alexandria - Psycho (Official Video)
+CASKETS - More Than Misery
+RHINO - Skid Row - Monkey Business (Official Music Video)
+Better Noise Music - From Ashes To New - Armageddon (Official Music Video)
+MONUMENTS - Opiate
+penguinz0 - GodSlap | Nefarious
+Self Deception - Self Deception - Fight Fire With Gasoline | Official video
+Better Noise Music - NOTHING MORE – SPIRITS (OFFICIAL LYRIC VIDEO)
+Miles Minnick - Miles Minnick - Glo Time (Official Video)
+Long Branch Records - Time, The Valuator - Binary Pulse (Official Video)
+Steamhammer - Time, The Valuator - Black Water (Official Video)
+Point North - Point North ft. The Ghost Inside - Safe And Sound
+Better Noise Music - From Ashes To New - Nightmare (Official Music Video)
+riserecords and Pale Chord Music - Spiritbox – The Void (Visualizer)
+Attack Attack! - Attack Attack! - Dark Waves
+SUMERIAN - VEIL OF MAYA - Red Fur (Official Music Video)
+Fixation - Fixation - More Alive (Official Music Video)
+Fixation - Fixation - Ignore The Disarray (Official Music Video)
+Ice Nine Kills - Ice Nine Kills - Ex-Mørtis
+Ice Nine Kills - Ice Nine Kills - Hip To Be Scared ft. Jacoby Shaddix (Official Music Video)
+Ice Nine Kills - Ice Nine Kills - The Shower Scene (Official Music Video) - Uncensored
+InVogue Records - Designer Disguise x Dropout Kings - Outta My Face (Vertical Video)
+SharpTone Records - We Came As Romans - Plagued
+riserecords - Crown The Empire - In Another Life (feat. Courtney LaPlante) (Official Music Video)
+riserecords - Crown The Empire - Immortalize (Official Music Video)
+Long Branch Records - Siamese - The Shape of Water feat. ten56. (Official Visualizer)
+SUMERIAN - ANIMALS AS LEADERS - Tooth and Claw
+⚡️NERV⚡️ - Nerv - "I Need Help" (Official Music Video)
+SUMERIAN - ANIMALS AS LEADERS - Ka$cade
+SUMERIAN - ANIMALS AS LEADERS - The Woven Web
+⚡️NERV⚡️ - Bad Nature
+Long Branch Records - Black Orchid Empire - Singularity (Visualiser)
+InSearchOfSunVEVO - In Search Of Sun - say it like you see it
+Our Last Night - Our Last Night - F.E.A.R. (Official Video)
+⚡️NERV⚡️ - Nerv - "Not The One" (Official Video)
+Hopeless Records - Point North - Into The Dark (Feat. Kellin Quinn) [Official Music Video]
+SharpTone Records - CASKETS - Lost In Echoes (Official Music Video)
+Colorblind - Colorblind - Love is Red (Official Music Video)
+Colorblind - Colorblind - Shameless
+Colorblind - Colorblind-Motionless
+Arising Empire - FROM FALL TO SPRING - Destiny (OFFICIAL VIDEO)
+From Fall to Spring - From Fall to Spring - Supernova (Official Lyric Video)
+From Fall to Spring - From Fall to Spring - RISE (Official Video)
+From Fall to Spring - From Fall to Spring - BR4INFCK (Official 4k Music Video)
+Arising Empire - FROM FALL TO SPRING - Black Heart (OFFICIAL VIDEO)
+CASKETS - One More Time
+Fixation - Fixation - Stay Awake (Official Music Video)
+MontageRock - Fighting Kind - Beginning Again [HD]
+Blue Stahli - Blue Stahli - Prognosis
+Hands Like Houses - Hands Like Houses - Hurricane
+Arising Empire - FROM FALL TO SPRING - Beastmode (OFFICIAL VIDEO)
+Chaosbay - Catch-22
+Chaosbay - CHAOSBAY - Y (Official Video)
+Chaosbay - Avalon
+Chaosbay - CHAOSBAY - What Is War (feat. SIAMESE) - Official Audio
+Chaosbay - CHAOSBAY - All This Beauty Can't Be Real (Official Video)
+Chaosbay - CHAOSBAY - 2 Billion (Official Video)
+Cultus Black - Cultus Black - Nevermine (Official Music Video)
+riserecords - Memphis May Fire - Over It
+VERSUS ME (VSME) - Versus Me - Terrified (Official Music Video)
+VERSUS ME (VSME) - Versus Me - Down (Official Music Video)
+SolenceOfficial - Solence - Rain Down (Official Music Video)
+Thriller Records - AVOID - Can't Take This Away (Official Music Video)
+Tommy Vext  - Tommy Vext - Cancel the King (Official Music Video)
+Tommy Vext  - Tommy Vext - The War You Wanted (Official Music Video -Starring Triple V)
+Tommy Vext  - Tommy Vext - Trust the Science feat. Topher [Official Music Video]
+Electric Callboy - Electric Callboy feat. @conquerdividemusic - FCKBOI (OFFICIAL VIDEO)
+Tommy Vext  - Tommy Vext - Cancel the King (Official Music Video)
+Self Deception - Self Deception - Stockholm Hearts | Official video
+Fame on Fire - Plastic Heart - Fame on Fire (Official Video)
+Self Deception - Self Deception - TicToc (Brace For Impact) Official Video
+Self Deception - Self Deception - Shade Of Dark (OFFICIAL LYRIC VIDEO)
+Self Deception - Self Deception The Blues (OFFICIAL LYRIC VIDEO)
+Better Noise Music - From Ashes To New ft. Matty Mullins - Until We Break
+Better Noise Music - From Ashes To New - Heartache (Official Music Video)
+Dayshell - Dayshell - Blindside Lyric video (Official)
+Ice Nine Kills - Ice Nine Kills - Hunting Season
+Fozzy - Fozzy - I Still Burn
+Muse - Muse - WON'T STAND DOWN (Official Video)
+Better Noise Music - Bad Wolves - The Body (OFFICIAL LYRIC VIDEO)
+Volbeat - Volbeat - Shotgun Blues (Official Music Video)
+Better Noise Music - The Retaliators – 21 Bullets ft. Motley Crue, Asking Alexandria, Ice Nine Kills & From Ashes To New
+riserecords - Memphis May Fire - Only Human (feat. AJ Channer)
+Nickelback - Nickelback - For The River [Audio]
+Self Deception - Self Deception - Legends (OFFICIAL LYRIC VIDEO)
+Papa Roach - Papa Roach - Kill The Noise [Official Music Video]
+Self Deception - Self Deception - PSYCHO | Official video
+Better Noise Music - Eva Under Fire - Comatose (feat. Jonathan Dörr of @EgoKillTalentMusic )
+Better Noise Music - Papa Roach - Broken As Me feat. Danny Worsnop of Asking Alexandria (Official Lyric Video)
+VRSTY - VRSTY - Massive
+Five Finger Death Punch - Five Finger Death Punch - AfterLife (Official Lyric Video)
+Better Noise Music - Bad Wolves - If Tomorrow Never Comes (Official Music Video)
+FALSET - FALSET - KINGDOM [OFFICIAL VIDEO]
+coldrain - coldrain - Before I Go (Official Music Video)
+Inner Echo - Inner Echo - The Demon Inside (Official Lyric Video)
+Official Alice Cooper - Alice Cooper - Feed My Frankenstein (Official Video)
+starsetonline - STARSET - Waiting On The Sky To Change (ft. Breaking Benjamin)
+Official Alice Cooper - Alice Cooper - Love's A Loaded Gun
+AVOIDKICKSASS - AVOID - Flashbang! (Official Music Video)
+Three Days Grace - Three Days Grace - So Called Life (Official Video)
+Three Days Grace - Three Days Grace - A Scar Is Born (Visualizer)
+SUMERIAN - SLEEPING WITH SIRENS - Crosses (ft. Spencer Chamberlain)
+ArteryRecordings - Siamese - Cities (Track Video)
+While She Sleeps - ENLIGHTENMENT (?)
+IPrevailBand - I Prevail - "Bad Things" (Official Music Video)
+Motionless In White - Motionless In White - Sign Of Life [Official Video]
+Motionless In White - Motionless In White - Sign Of Life [Official Video]
+Motionless In White - Motionless In White - Werewolf [Official Video]
+coldrain - coldrain - PARADISE(Kill The Silence) 【Official Music Video】
+Fearless Records - The Word Alive - Trapped (Official Music Video)
+While She Sleeps - EYE TO EYE - While She Sleeps (Official Video)
+SharpTone Records - Savage Hands - Blue (OFFICIAL MUSIC VIDEO)
+ENMY - ENMY - Deceiver (Official Music Video)
+Motionless In White - Motionless In White - Scoring The End Of The World (Feat. Mick Gordon) [Official Audio]
+riserecords - Memphis May Fire - The American Dream (Visualizer)
+Solstate - Solstate - The Void
+Epitaph Records - Falling In Reverse - "Voices In My Head"
+Steamhammer - Siamese - Holy (Official Video)
+Siamese - Topic - Heights Above
+Polyphia - Polyphia - Playing God (Official Music Video)
+Long Branch Records - Siamese - Home feat. Drew York / Stray From The Path (Official Video)
+riserecords - Memphis May Fire - Left For Dead (Visualizer)
+Epitaph Records - Architects - "when we were young"
+DAMN NATION - DAMN NATION - 'Would You Stay' (Official Music Video)
+Jared Dines - Daddy Rock - "The River Song" (Official Music Video)
+riserecords - Hands Like Houses - Colourblind (Official Music Video)
+Epitaph Records - Architects - "Libertine" (Full Album Stream)
+riserecords - Sevendust - All I Really Know
+riserecords - Sevendust - Blood From A Stone
+Better Noise Music - NOTHING MORE - TURN IT UP LIKE (Stand In The Fire) [Official Music Video]
+riserecords - Memphis May Fire - Bleed Me Dry (Visualizer)
+Epitaph Records - Architects - "Dead Butterflies"
+Epitaph Records - Architects - "An Ordinary Extinction" (Full Album Stream)
+Jake Howsam Lowe - I Built The Sky & Jake Howsam Lowe - Stonewalker
+I Built The Sky - I Built The Sky & Jake Howsam Lowe - Coalesce
+Plini - Plini - Cascade (Audio)
+starsetonline - STARSET - Brave New World (Official Music Video)
+depth of self-delusion - riverside
+castaway angel - leprous
+hold on - leprous
+TEMIC - Acts of Violence
+Aeon Spoke - The Fisher Tale
+OSI - Go
+Agent Fresco - The Autumn Red
+RPWL - Welcome to the Freak Show
+Riverside - In Two Minds - Remastered
+Kingcrow - Glitch
+Cold Night For Alligators - Hindsight
+Maraton - In Syzygy
+Einar Solberg - Over the Top
+Wilderun - Exhaler
+Kingcrow - Everything Goes
+Soen - Lucidity
+TesseracT - Tender
+Agent Fresco - Angst
+Frost* - Skywards
+Klone - Sealed
+Leprous - Stuck
+Anathema - The Lost Child
+Pain of Salvation - WAIT
+The Ocean - Bathyalpelagic I: Impasses
+Daniel Cavanagh - This Music
+The Black Noodle Project - Left Behind
+Katatonia - City Glaciers
+Port Noir - Black from the Ink
+Opeth - In My Time of Need
+Arena - How Did It Come to This?
+Big Big Train - Folklore
+White Moth Black Butterfly  - Bloom (feat. Eric Guenther)
+Conception - Feather Moves - Remastered
+Leprous - Faceless
+Vulkan - The Madness Sees No End
+Klone - Scarcity
+Green Carnation - Transparent Me - Remastered
+Mother of Millions - Shine
+Mariusz Duda - Are You Ready for the Sun
+Katatonia - Pale Flag
+RPWL - The Ugliest Man in the World
+The Mayan Factor - Self Storage
+White Moth Black Butterfly - Ties of Grace
+Haken - Taurus
+Leprous - The Weight of Disaster
+Wolverine - Communication Lost
+Rendezvous Point - Pressure
+Riverside - Deprived (Irretrievably Lost Imagination)
+Agent Fresco - Let Fall the Curtain
+Oak - False Memory Archive
+Wolverine - Into The Great Nothing
+Caligula's Horse - The Stormchaser
+Leprous - Forced Entry
+VOLA - Freak
+Evergrey - Where August Mourns
+Haken - As Death Embraces
+Einar Solberg  - Blue Light (feat. Asger Mygind)
+Riverside - Before
+Oceans of Slumber - Hearts of Stone
+Votum - Prey
+Ihlo - Source
+RPWL - We Are What We Are
+Rishloo - Radio
+Persefone - Katabasis
+Leprous - I Hear the Sirens
+Vulkan - The Luning
+Votum - Horizontal
+Earthside  - The Lesser Evil
+Soen - Monarch
+Amarok - Surreal
+Leprous - The Silent Revelation
+Weather Systems - Synaesthesia
+Wheel - It's Over Now
+Vulkan - Hypnotica
+Airbag - Killer
+VOLA - Head Mounted Sideways
+Leprous - Lower
+TesseracT - Juno - P O R T A L S
+Ghost Brigade - Into The Black Light
+Textures - Timeless
+IQ - Rise
+VOLA - Napalm
+Agent Fresco - Howls
+White Moth Black Butterfly - Rising Sun
+AVKRVST - The Pale Moon
+Ihlo - Legacy
+Riverside - Loose Heart - Remastered
+Bjørn Riis - Where Are You Now
+Votum - Spiral
+IQ - Frequency
+White Moth Black Butterfly - Ether
+TEMIC - Falling Away
+Zeal & Ardor - Golden Liar
+Airbag - Broken
+Frost* - Waiting for the Lie
+Lunatic Soul - The Passage
+Leprous - Illuminate
+Vulkan - Blinding Ornaments
+OSI - Blood
+TesseracT - Tourniquet
+Pure Reason Revolution - Silent Genesis
+Steven Wilson - Refuge
+Steven Wilson - What Life Brings
+ castaway angels - leprous
+Riverside - Acronym Love
+Blackfield - My Gift of Silence
+Leprous - Out of Here
+Ihsahn - Roads
+Iamthemorning - Matches
+Port Noir - Any Way the Wind Carries
+Haken - Deathless - remastered 2017
+Gazpacho - Winter Is Never
+Soen - Antagonist
+Blackfield - From 44 to 48
+Pain of Salvation - ACCELERATOR
+TesseracT - Juno
+Bruce Soord  - Pleasure
+Porcupine Tree - Shesmovedon
+Leprous - Have You Ever?
+A.A. Williams - Melt
+Trees of Eternity - My Requiem
+Haken - Lovebite
+Soup - Going Somewhere
+Riverside - After
+Bruce Soord - Time Does Not Exist
+Katatonia - Shifts
+Opeth - Will O the Wisp
+Leprous - Atonement
+Alcest - Délivrance
+Port Noir - Debris
+Soen - Covenant
+Kingcrow - Drenched
+Pure Reason Revolution - Apprentice of the Universe
+Opeth - Lovelorn Crime
+Vulkan - Redemption Simulations
+Antimatter - Paranova
+Riverside - Stuck Between
+Airbag - Feeling Less
+Flying Colors - More
+Mother of Millions - Anchor
+Leprous - On Hold
+Green Carnation - Alone
+Anekdoten - Get out Alive
+Lunatic Soul - Adrift
+Opeth - Coil
+Klone - Immersion
+The Mayan Factor - Focus - Reborn
+Chroma Key - America The Video
+VOLA - Vertigo
+Soen - EMDR
+Riverside - Afloat
+Katatonia - Untrodden
+The Pineapple Thief - Alone at Sea
+Kingcrow - Closer
+Vulkan - Spökskepp
+The Ocean - Mesopelagic: Into the Uncanny
+Blind Ego - Never Escape the Storm
+Opeth - Benighted (Remastered)
+Riverside - Vale of Tears
+Vulkan - From Here To Where It All Ends (Terminus)
+Ali Ferguson - Out Of The Dark
+Haken - Sun - remastered 2017
+Leprous - Alleviate
+Soen - Lotus
+Airbag - Machines and Men
+Ihlo - Reanimate
+Leprous - Silhouette
+Chroma Key - Astronaut Down
+Skysketch - Fox Wedding
+Our Oceans - Weeping Lead
+White Moth Black Butterfly - Tempest
+Rishloo - Keyhole in the Sky
+Artificial Language - The Back of My Mind
+Lunatic Soul - Cold
+Antimatter - No Contact
+Woods Of Ypres - Finality
+Hypno5e - Night on the Petrified Sea
+Subsignal - A Room on the Edge of Forever
+The Butterfly Effect - Worlds On Fire
+Ayreon - Computer Eyes
+Big Big Train - The First Rebreather
+Steven Wilson  - Pariah
+Ions - True Friendship
