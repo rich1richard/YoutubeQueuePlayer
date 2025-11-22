07cc2f276715426250bb557a9409f3c180dd4416
rich1richard <gamedevrichard@gmail.com>
2025-11-22 15:05:25 +0100

    - Fix the bug when adding a song to queue - Rename and organize playlists in the folder 'Playlists'

---
+++ Playlists/Afro House Vibes.pl
@@ -0,0 +1,29 @@
+Francis Mercier & Magic System - Premier Gaou (Nitefreak Remix)
+Lana del Rey - Summertime Sadness (Zeno Altea Afro House Remix)
+The Weeknd, Playboi Carti - Timeless (Tasty Or Not Remix) [Afro House]
+Adam Port, Stryv - Move feat. Malachiii (Extended Version)
+Zerb - Mwaki (Feat. @Sofiya Nzau) [Official Music Video]
+Haddaway - What Is Love (KYOMA Afro House Remix)
+David Guetta ft. Sia - Titanium (Vani Afro House Remix)
+DJ BeOne - Mas Que Nada (Afro House Remix)
+Ahmed Spins feat Stevo Atambire - Anchor Point
+Sultan + Shepard - Assassin
+Disclosure - You & Me (Rivo Remix) ft. Eliza Doolittle
+Shimza x AR/CO x Kasango - Fire Fire (Visualizer) [Helix Records]
+Tayna - Si Ai (Marshmello & UKAY Remix)
+Jazzy, KILIMANJARO - No Bad Vibes
+Meliora - Pendiente A Mi [Afro House]
+Francis Mercier, Faul & Wad, African Children's Choir - Sauti
+Naika - 1+1 (BLK Afro House Remix)
+Nearly - What I Need
+Tasty Or Not - Oh La La La (Hot Summer Night) [Afro House]
+Haska - Madan
+Naughty Boy, Sam Smith - La la la (Lumont Afro House Remix)
+Naomi Sharon - Another Life (Marten Lou Remix)
+Andor Gabriel, Jerome Sydor - Temperatura [Latin Afro House]
+TitoM, Yuppe and Burna Boy - Tshwala Bam Remix [Ft. S.N.E]
+Francis Mercier, Nitefreak, Idd Aziz - Kamili (Lyrics)
+Nitefreak & Emmanuel Jal - Gorah (Official Full Stream)
+MoBlack - Yamore (Corona Sunsets Session)
+Awen & Caiiro - Your Voice (Adam Port Remix)
+Monique Bingham, Black Coffee - Deep In The Bottom (of Africa)
---
+++ Playlists/Old French.pl
@@ -0,0 +1,102 @@
+Daniel Balavoine - L'Aziza
+Gold - Ville de lumière (2017 - Remaster)
+Gold - Pour toi la musique (2017 - Remaster)
+Hervé Cristiani - Il Est Libre Max
+Axelle Red - Rester femme
+Vladimir Cosma  - Destinée
+Images - L'enfant des rizières
+Mike Brant - Tout donné, tout repris - Remasterisé en 2010
+Pierre Rapsat - Adeu
+Gold - Calicoba (2017 - Remaster)
+Julien Clerc - Femmes... Je vous aime
+Joe le taxi - La gitane (ma tête tourne)
+Jean-Luc Lahaye - Papa chanteur
+Pierre Bachelet - Elle est d'ailleurs
+Bonnie Tyler  - Si demain... (Turn round)
+Axelle Red - Parce que c'est toi
+Karen Cheryl - A l'envers à l'endroit
+Pierre Rapsat - Les rêves sont en nous
+Joe Dassin - La fleur aux dents
+Elsa - Jour de neige
+Pierre Bachelet - Partis avant d'avoir tout dit
+Gold - T'es pas fou
+Patrick Fiori - Je sais où aller
+Jakie Quartz - A la vie, à l'amour
+Gold - Capitaine abandonné (Live) ( 2017 - Remaster)
+Michel Berger - Les princes des villes - Remasterisé en 2002
+Laroche Valmont - T'as le look coco - Radio Edit
+Demis Roussos - On Écrit Sur Les Murs
+Images - Maîtresse (Version radio)
+Téléphone - Ça c'est vraiment toi - Remasterisé en 2006
+Gold - Des matins bleus (2017 - Remaster)
+Patricia Kaas - Je voudrais la connaître
+Philippe Lafontaine - Alexis m'attend
+Nilda Fernandez - Nos fiançailles
+Gold - Crise d'amour cardiaque
+Zouk Machine - Maldòn - Version originale
+Christophe Willem - Je marche seul
+Claude Barzotti - Je ne t'écrirai plus
+Mike Brant - Serre les poings et bats-toi
+Gold - Sans façon
+Patrick Fiori - Yoenaï
+Véronique Sanson - Ma révérence - Remasterisé en 2008
+Philippe Lavil - De Bretagne ou d'ailleurs
+Gérard Blanc - Du soleil dans la nuit
+Pierre Bachelet - En l'an 2001
+Gérard Lenorman - Si j'étais président - 2023 Remaster
+Léopold Nord & Vous - C'est l'amour - Version originale
+France Gall - Il jouait du piano debout - Remasterisé en 2004
+Gérard Blanc - Sentiment d'océan
+Patrick Fiori - Sans bruit
+Images - Le cœur en exil
+Laurent Voulzy - Les nuits sans Kim Wilde
+Jeanne Mas - Sauvez-moi
+Renaud - La pêche à la ligne
+Michel Berger - Quelques mots d'amour - Remasterisé en 2002
+Ottawan - T'es OK, T'es Bath, T'es In
+Grégoire - Ta main
+Philippe Lafontaine - Bibi débraye
+Philippe Lavil - Elle préfère l'amour en mer
+Michel Berger - Chanter pour ceux qui sont loin de chez eux - Remasterisé en 2002
+Gold - Josy-Ann (2017 - Remaster)
+Christophe Willem - Double je - Remix
+Félix Gray - La gitane
+Peter & Sloane - Besoin de rien, envie de toi
+Francis Cabrel - C'est écrit - Remastered
+Julie Pietri - Et c'est comme si
+Jean-Pierre Mader - Macumba
+Pierre Bachelet - Marionnettiste
+Thierry Pastor - Le coup de folie
+Axelle Red - Sensualité
+Patrick Bruel - J'te l'dis quand même
+Christine Roques - Premiers frissons d'amour
+Gold - Spleen (2017 - Remaster)
+Hugues Aufray - Tchin tchin
+Véronique Sanson - Amoureuse - Remasterisé en 2008
+David Hallyday - Ma dernière lettre
+Jean-Jacques Goldman - Puisque tu pars
+Mylène Farmer - Rêver
+Daniel Guichard - Mon vieux
+Renaud - Manu
+De Palmas - Une seule vie (Marcher dans le sable)
+Daniel Balavoine - Petit homme mort au combat
+Claude François - Si j'avais un marteau
+Bernard Lavilliers  - Idées noires
+Catherine Lara - Au Milieu De Nulle Part
+Renaud - Morgane de toi
+Nino Ferrer - Le sud
+Jean-Luc Lahaye - Pleure Pas
+Serge Lama - Je suis malade
+William Sheller - Les filles de l'aurore
+Catherine Lara - La rockeuse de diamants
+Eddy Mitchell - La dernière séance
+Charts - Notre Monde A Nous
+Gilbert Montagné - Tes Quinze Ans Ton Sourire
+Daniel Balavoine - Le chanteur
+Niagara - Soleil d'hiver
+Daniel Balavoine - Revolucion
+Raft - Yaka danse
+Dalida - Monday, Tuesday... Laissez-moi danser
+Daniel Balavoine - Soulève-moi
+Mylène Farmer - Regrets
+Francis Lalanne - Reste avec moi
